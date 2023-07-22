4059dcc1f03619b168452ef39449f5c86dd41ebf
Daniel Wagner-Hall <dawagner@gmail.com>
2023-07-22 11:34:42 +0100

    Add request counter in log

--- server.js
+++ server.js
@@ -4,7 +4,11 @@ const app = express();
 
 const port = 3000;
 
+let requestCount = 0;
+
 app.get("/", (request, response) => {
+    requestCount = requestCount + 1;
+    console.log(`Got request ${requestCount}`);
     response.send("Hello");
 });
 

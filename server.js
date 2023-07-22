4a4b50ef3571d6820197351c7bbe6d92cde69443
Daniel Wagner-Hall <dawagner@gmail.com>
2023-07-22 11:03:30 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+const port = 3000;
+
+app.get("/", (request, response) => {
+    response.send("Hello");
+});
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+})
